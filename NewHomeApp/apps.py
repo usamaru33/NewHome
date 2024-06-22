454358dbc8760f1ab605f98c0c209b3982867f5f
usamaru33 <raioraioraio15@gmail.com>
2024-06-22 16:56:46 +0900

    8

--- NewHomeApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class NewhomeappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'NewHomeApp'
--- NewHomeApp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views import generic
-from NewHomeApp.models import Users
-
-class UserListView(generic.ListView):
-    model = Users
-    template_name = 'user_list.html'
-    context_object_name = 'users'
-
