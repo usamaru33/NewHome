432cfdc0efc97454f58a9f2d436205b7e71b99eb
usamaru33 <raioraioraio15@gmail.com>
2024-06-22 18:16:37 +0900

    9

---
+++ NewHomeApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NewhomeappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'NewHomeApp'
---
+++ NewHomeApp/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login
+from django.views import generic
+from django.contrib import messages
+from NewHomeApp.models import NewHomeApp
+
+class UserListView(generic.ListView):
+    model = NewHomeApp
+    template_name = 'NewHome.html'
+    context_object_name = 'users'
+
+# ログイン後に表示するページ
+def home_page(request):
+    return render(request, 'Edit.html')
+
+# ログイン時に表示するページとログイン機能
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # ユーザー認証
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            # ログイン成功
+            auth_login(request, user)
+            return redirect('home')
+        else:
+            # ログイン失敗
+            messages.error(request, 'ユーザー名またはパスワードが間違っています。')
+
+    return render(request, 'Login.html')
