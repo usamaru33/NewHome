714a1993d656856b8b71c24e2f759533b3e21551
usamaru33 <raioraioraio15@gmail.com>
2024-06-22 17:19:13 +0900

    1

--- NewHomeApp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class NewHomeApp(models.Model):
-    name = models.CharField(max_length=255)
-    title = models.CharField(max_length=255)
-    contents = models.TextField()
-
-    def __str__(self):
-        return self.title
